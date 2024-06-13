ad3036f82a482196e4c821f28a2ff836656b446c
Edison Teran <javaedi.et@gmail.com>
2024-06-13 17:21:33 -0500

    refactoring name

--- clientes/src/test/java/ec/editer/clientes/ClientesApplicationTests.java
+++ clientes/src/test/java/ec/editer/clientes/ClientesApplicationTests.java
@@ -25,7 +25,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 @SpringBootTest
-class MicroserviceAApplicationTests {
+class ClientesApplicationTests {
     
     @Autowired
     private ResourceLoader resourceLoader;
--- cuentas/src/test/java/ec/editer/cuentas/CuentasApplicationTests.java
+++ cuentas/src/test/java/ec/editer/cuentas/CuentasApplicationTests.java
@@ -3,6 +3,6 @@ package ec.editer.cuentas;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroserviceBApplicationTests {
+class CuentasApplicationTests {
 
 }
